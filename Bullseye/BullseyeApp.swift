93c4b6fed900bf9f765c73ba7b7446652cc1aab5
Navinda Lankesh <adilanga@gmail.com>
2022-03-11 12:55:09 +0530

    Project Init

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by m1 on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
